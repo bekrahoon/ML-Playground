eb90be7231e61e09571a946f72917ca8cee773b8
Bekrahun <bekrahoon12@gmail.com>
2025-11-30 14:08:05 +0600

    изменено:      accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
