4404266920604c577f869252383fd866db96e347
Bekrahun <bekrahoon12@gmail.com>
2025-11-30 13:46:02 +0600

    изменено:      datasets/admin.py

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Dataset, MLModel
 
-# Register your models here.
+
+@admin.register(Dataset)
+class DatasetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner', 'rows_count', 'columns_count', 'created_at']
+    list_filter = ['created_at', 'owner']
+    search_fields = ['name', 'description']
+    readonly_fields = ['created_at', 'updated_at', 'rows_count', 'columns_count']
+
+
+@admin.register(MLModel)
+class MLModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'algorithm', 'dataset', 'owner', 'accuracy', 'r2_score', 'created_at']
+    list_filter = ['algorithm', 'created_at', 'owner']
+    search_fields = ['name', 'description']
+    readonly_fields = ['created_at']
